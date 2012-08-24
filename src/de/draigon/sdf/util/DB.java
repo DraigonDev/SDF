2e5aa8a61b03415daf8551a5dab6cc9275cc6117
Draigon <git@draigon.de>
2012-08-24 12:15:49 +0200

    Javadoc

--- src/de/draigon/sdf/util/DB.java
+++ src/de/draigon/sdf/util/DB.java
@@ -1,15 +1,37 @@
 package de.draigon.sdf.util;
 
-import de.draigon.sdf.connection.ConnectionProperties;
+import java.io.IOException;
 
+import de.draigon.sdf.connection.ConnectionProperties;
+import de.draigon.sdf.exception.DBException;
+
+/**
+ * DB properties. this class accesses the escape sequence for embedded or not
+ * embedded databases. the embadded database cant work with keys escaped via "`"
+ * - on normal databases it should be used, to prevent function usage from key
+ * accesses
+ * 
+ * @author Draigon Development
+ * @version 1.0
+ */
 public class DB {
-    public static String ESCAPE = "`";
-    
-    static{
-        if (ConnectionProperties.get().isEmbedded()) {
-            DB.ESCAPE = "";
-        } else {
-            DB.ESCAPE = "`";
-        }
-    }
+	/**
+	 * the database specific escape string
+	 */
+	public static String ESCAPE = "`";
+
+	/**
+	 * initiates the key from the properties embedded value
+	 */
+	static {
+		try {
+			if (ConnectionProperties.get().isEmbedded()) {
+				DB.ESCAPE = "";
+			} else {
+				DB.ESCAPE = "`";
+			}
+		} catch (IOException e) {
+			throw new DBException("unable to access properties for DB-decision");
+		}
+	}
 }
