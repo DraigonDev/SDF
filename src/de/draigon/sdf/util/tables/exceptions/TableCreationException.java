e3c20334175b00e4674299a9560a280e5955d864
Draigon <git@draigon.de>
2012-09-03 10:12:10 +0200

    deleted comments

--- src/de/draigon/sdf/util/tables/exceptions/TableCreationException.java
+++ src/de/draigon/sdf/util/tables/exceptions/TableCreationException.java
@@ -2,29 +2,22 @@ package de.draigon.sdf.util.tables.exceptions;
 
 public class TableCreationException extends RuntimeException{
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 3889323921567052369L;
 
     public TableCreationException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public TableCreationException(String arg0, Throwable arg1) {
         super(arg0, arg1);
-        // TODO Auto-generated constructor stub
     }
 
     public TableCreationException(String arg0) {
         super(arg0);
-        // TODO Auto-generated constructor stub
     }
 
     public TableCreationException(Throwable arg0) {
         super(arg0);
-        // TODO Auto-generated constructor stub
     }
     
 }
